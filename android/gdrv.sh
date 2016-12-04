161995b7c3d02273b56a5e2261f9ade530c21650
yumaokao <ymkq9h@gmail.com>
2016-12-04 09:53:46 +0800

    gdrv check with grep book id only

--- android/gdrv.sh
+++ android/gdrv.sh
@@ -6,7 +6,11 @@ GDRV_PDF_FILES=$(gdrv list "$GDRV_PDF_DIR")
 
 cd $LOCAL_PDF_DIR && ls | while read B
 do
+    BID=${B##*-}
+    BID=${BID%%.pdf}
+    # echo "$BID"
+
     echo -n "$B... "
-    echo $GDRV_PDF_FILES | grep -q "$B" && echo "Already" || gdrv push "$B" $GDRV_PDF_DIR
+    echo $GDRV_PDF_FILES | grep -q "$BID" && echo "Already" || gdrv push "$B" $GDRV_PDF_DIR
     # gdrv list $GDRV_PDF_DIR | grep "$B" > /dev/null && echo "Already" || gdrv push $GDRV_PDF_DIR "$B"
 done
