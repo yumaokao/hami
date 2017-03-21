6f52559d16bf4154175c46460ff20fc142702b25
yumaokao <ymkq9h@gmail.com>
2017-03-21 19:48:26 +0800

    [hamiui] launch hamibook

--- android/hamiui/app/src/androidTest/java/tw/ymk/apk/hamiui/ExampleInstrumentedTest.java
+++ android/hamiui/app/src/androidTest/java/tw/ymk/apk/hamiui/ExampleInstrumentedTest.java
@@ -1,16 +1,22 @@
 package tw.ymk.apk.hamiui;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.filters.SdkSuppress;
+import android.support.test.uiautomator.Until;
 
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.*;
 
 /**
@@ -22,7 +28,9 @@ import static org.junit.Assert.*;
 @SdkSuppress(minSdkVersion = 18)
 public class ExampleInstrumentedTest {
 
+    private static final String BASIC_SAMPLE_PACKAGE = "com.she.eReader";
     private UiDevice mDevice;
+    private static final int LAUNCH_TIMEOUT = 5000;
 
     @Before
     public void startHamiActivityFromHomeScreen() {
@@ -30,6 +38,20 @@ public class ExampleInstrumentedTest {
         mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
         // Start from the home screen
         mDevice.pressHome();
+        // Wait for launcher
+        final String launcherPackage = getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
+
+        // Launch the hamibook app
+        Context context = InstrumentationRegistry.getContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);    // Clear out any previous instances
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        mDevice.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)), LAUNCH_TIMEOUT);
     }
 
     @Test
@@ -43,4 +65,21 @@ public class ExampleInstrumentedTest {
     public void openHamiBook() throws Exception {
 
     }
+
+    /**
+     * Uses package manager to find the package name of the device launcher. Usually this package
+     * is "com.android.launcher" but can be different at times. This is a generic solution which
+     * works on all platforms.`
+     */
+    private String getLauncherPackageName() {
+        // Create launcher Intent
+        final Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+
+        // Use PackageManager to get the launcher package name
+        PackageManager pm = InstrumentationRegistry.getContext().getPackageManager();
+        ResolveInfo resolveInfo = pm.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return resolveInfo.activityInfo.packageName;
+    }
+
 }
