6362d3f65db1eedc38d1e134b428172b21cc2361
yumaokao <ymkq9h@gmail.com>
2017-03-21 17:48:02 +0800

    add uiautomator dependencies

--- android/hamiui/app/src/androidTest/java/tw/ymk/apk/hamiui/ExampleInstrumentedTest.java
+++ android/hamiui/app/src/androidTest/java/tw/ymk/apk/hamiui/ExampleInstrumentedTest.java
@@ -3,7 +3,11 @@ package tw.ymk.apk.hamiui;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.filters.SdkSuppress;
 
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -15,12 +19,28 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
 public class ExampleInstrumentedTest {
+
+    private UiDevice mDevice;
+
+    @Before
+    public void startHamiActivityFromHomeScreen() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        // Start from the home screen
+        mDevice.pressHome();
+    }
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("tw.ymk.apk.hamiui", appContext.getPackageName());
+    }
+
+    @Test
+    public void openHamiBook() throws Exception {
+
     }
 }
