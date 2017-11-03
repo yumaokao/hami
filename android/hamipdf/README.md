fa66e4c4d2d73185f5e786392ade94a0e7f7fd19
yumaokao <ymkq9h@gmail.com>
2017-11-03 16:24:41 +0800

    add hamipdf

---
+++ android/hamipdf/README.md
@@ -0,0 +1,9 @@
+# hamipdf
+
+## requires
+
+``` sh
+$ python -m venv venv
+$ source venv/bin/activate
+$ pip install pdfrw
+```
---
+++ android/hamipdf/hamipdf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+
+import os
+import argparse
+from pdfrw import PdfReader
+from pdfrw.findobjs import find_objects
+
+VERSION = '0.2.0'
+
+
+def main():
+    parser = argparse.ArgumentParser(description='hamipdf')
+    parser.add_argument('-v', '--verbose', help='show more debug information', action='count', default=0)
+    parser.add_argument('-V', '--version', action='version', version=VERSION, help='show version infomation')
+    parser.add_argument('pdfs', nargs='+', metavar='PDF', help='pdf files')
+    args = parser.parse_args()
+
+    # print(args.pdfs)
+    pdf = PdfReader(args.pdfs[0])
+    # for obj in find_objects(pdf.pages):
+    #     print(obj)
+    # import ipdb
+    # ipdb.set_trace()
+    for obj in find_objects(pdf.pages[1]):
+        print(obj)
+
+
+if __name__ == "__main__":
+    main()
