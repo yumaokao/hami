9be33da41e3673cb354c4c00e98ae93bb1e67e57
yumaokao <ymkq9h@gmail.com>
2018-01-21 15:58:10 +0800

    hamifire: attach listFiles after promise

--- android/hamifire/hamifire.js
+++ android/hamifire/hamifire.js
@@ -4,6 +4,7 @@
 
 const util = require('util');
 const fs = require('fs');
+const google = require('googleapis');
 const googleAuth = require('google-auth-library');
 
 const readFile = util.promisify(fs.readFile);
@@ -17,11 +18,36 @@ function authorize(credentials) {
     var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
     readFile('credentials.json')
       .catch(error => console.log(error))
-      .then(token => console.log(JSON.parse(token)));
+      .then(token => oauth2Client.credentials = JSON.parse(token))
+      .then(() => resolve(oauth2Client));
+  });
+}
+
+function listFiles(auth) {
+  var service = google.drive('v3');
+  service.files.list({
+    auth: auth,
+    pageSize: 10,
+    fields: "nextPageToken, files(id, name)"
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var files = response.files;
+    if (files.length == 0) {
+      console.log('No files found.');
+    } else {
+      console.log('Files:');
+      for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        console.log('%s (%s)', file.name, file.id);
+      }
+    }
   });
 }
 
 readFile('client_secret.json')
   .catch(error => console.log(error))
-  .then(file => authorize(JSON.parse(file)));
-
+  .then(file => authorize(JSON.parse(file)))
+  .then(auth => listFiles(auth));
