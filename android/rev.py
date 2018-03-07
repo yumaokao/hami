eb42d9107e0821760eceec08df69d35ca9f2f15a
yumaokao <ymkq9h@gmail.com>
2018-03-07 11:59:31 +0800

    hami: rev.py maintain a local db

--- android/rev.py
+++ android/rev.py
@@ -1,10 +1,11 @@
 # vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
 
 import os
+import json
 import shutil
+import subprocess
 from os import path
 from lxml import etree
-import subprocess
 
 EXTRACTS_PATH = "/storage/emulated/0/Android/data/com.she.eReader/.hamibookEx/extracts/"
 GDRV_PDF_DIR = "/publics/hamis/最新/"
@@ -78,40 +79,47 @@ def reverse_pdf(b):
 
 def push_pdf(b, hamis):
     if not path.isdir(path.join(EXTRACTS_PATH, b)):
-        return False
+        return (False, None)
     fn_xml = path.join(EXTRACTS_PATH, b, 'meta.xml')
     if not path.isfile(fn_xml):
-        return False
+        return (False, None)
 
     # get title
     title = get_meta_title(fn_xml)
-    fn_title = path.join(EXTRACTS_PATH, b, '{}-{}.pdf'.format(title, b))
+    booknameid = '{}-{}.pdf'.format(title, b)
+    fn_title = path.join(EXTRACTS_PATH, b, booknameid)
 
-    print('pushing {}-{}.pdf: '.format(title, b), end='')
+    print('pushing {}: '.format(booknameid), end='')
 
     if not path.isfile(fn_title):
-        return False
+        return (False, None)
+
+    # check already in json
+    if len(list(filter(lambda d: b in d['name'], hamis['json']))) > 0:
+        print('Already pushed (json)')
+        return (True, booknameid)
+
+    # check already in recent
+    if len(list(filter(lambda d: b in d, hamis['hami']))) > 0:
+        print('Already pushed (recent)')
+        return (True, booknameid)
 
     # push to hami recent directory
-    if len(list(filter(lambda d: b in d, hamis['hami']))) > 0:
-        print('Already pushed')
-    else:
-        subprocess.check_call(['gdrv', 'push', fn_title, GDRV_PDF_DIR])
+    print()
+    subprocess.check_call(['gdrv', 'push', fn_title, GDRV_PDF_DIR])
+    return (Ture, booknameid)
 
-    '''
-    if len(list(filter(lambda n: n in title, DAILY_NEWSPAPERS))) > 0:
-        # push to newspaper directory
-        if len(list(filter(lambda d: b in d, hamis['daily']))) > 0:
-            print('Already pushed')
-        else:
-            subprocess.check_call(['gdrv', 'push', fn_title, '{}{}/'.format(GDRV_PDF_DIR, '報紙')])
-    else:
-        # push to hami directory
-        if len(list(filter(lambda d: b in d, hamis['hami']))) > 0:
-            print('Already pushed')
-        else:
-            subprocess.check_call(['gdrv', 'push', fn_title, GDRV_PDF_DIR])
-    '''
+
+def save_hami_json(hamis, pushs):
+    books = hamis['json']
+    print('讀取全部: {}'.format(len(books)))
+    pushs = list(filter(lambda p: p[0], pushs))
+    pushs = list(filter(lambda p: len(list(filter(lambda d: p[1] in d['name'], books))) == 0, pushs))
+    pushs = list(map(lambda p: {'name': p[1]}, pushs))
+    books = pushs + books
+    print('寫入全部: {}'.format(len(books)))
+    with open('/data/local/tmp/hami.json', 'w') as f:
+        json.dump(books, f)
 
 
 def get_gdrv_hami_list(path):
@@ -119,9 +127,12 @@ def get_gdrv_hami_list(path):
 
     books = subprocess.check_output(['gdrv', 'list', path])
     hamis['hami'] = [b.decode('utf-8') for b in books.splitlines()]
-    # news = subprocess.check_output(['gdrv', 'list', '{}{}/'.format(path, '報紙')])
-    # hamis['daily'] = [n.decode('utf-8') for n in news.splitlines()]
 
+    # local database
+    with open('/data/local/tmp/hami.json', 'r') as f:
+        books = json.load(f)
+    hamis['json'] = books
+    # print(json.dumps(books, indent=2))
     return hamis
 
 
@@ -140,11 +151,11 @@ def main():
     books.sort()
     list(map(lambda b: reverse_pdf(b), books))
     hamis = get_gdrv_hami_list(GDRV_PDF_DIR)
-    list(map(lambda b: push_pdf(b, hamis), books))
+    pushs = list(map(lambda b: push_pdf(b, hamis), books))
+    save_hami_json(hamis, pushs)
     list(map(lambda b: reverse_epub(b), books))
     if len(books) > 80:
         remove_old_books(books)
-
 
 
 if __name__ == "__main__":
