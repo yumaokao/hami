3d2fd91671de99f5565a384ea7b07fe0fce0c13d
yumaokao <yumaokao@gmail.com>
2018-07-31 19:03:17 +0800

    hamiorg: add class HamiRevOrg

--- android/hamiorg/hamirev.py
+++ android/hamiorg/hamirev.py
@@ -1,64 +1,88 @@
 # vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
 
 import os
+import csv
 import json
 import shutil
 from os import path
 from lxml import etree
 
 EXTRACTS_PATH = "/storage/emulated/0/Android/data/com.she.eReader/.hamibookEx/extracts/"
-
-def get_meta_title(fn_xml):
-    title = ''
-    with open(fn_xml) as inf:
-        root = etree.parse(inf).getroot()
-        titles = list(filter(lambda c: c.tag == 'bookname', root))
-        if len(titles) != 1:
-            return False
-        title = titles[0].text.replace('/', '.')
-        title = title[1:] if title[0] == '.' else title
-        # print(title)
-    return title
+HAMIUI_PATH = "/storage/emulated/0/Android/data/tw.ymk.apk.hamiui/files"
 
 
-def reverse_pdf(b):
-    if not path.isdir(path.join(EXTRACTS_PATH, b)):
-        return False
-    fn_pdf = path.join(EXTRACTS_PATH, b, b + '.pdf')
-    fn_dat = path.join(EXTRACTS_PATH, b, b + '.pdf.dat')
-    fn_xml = path.join(EXTRACTS_PATH, b, 'meta.xml')
-    if not path.isfile(fn_pdf) or not path.isfile(fn_dat) or not path.isfile(fn_xml):
-        return False
+class HamiRevOrg:
+    def __init__(self):
+        self.bookdirs = os.listdir(EXTRACTS_PATH)
+        self.bookdirs.sort()
+        self.revbooks = []
 
-    print('looking {}: '.format(b), end='')
-    # get title
-    title = get_meta_title(fn_xml)
-    fn_title = path.join(EXTRACTS_PATH, b, '{}-{}.pdf'.format(title, b))
+    def get_downloaded_csv(self):
+        fn_csv = path.join(HAMIUI_PATH, 'downloaded-episodes.csv')
+        if not path.isfile(fn_csv):
+            raise FileNotFoundError('{} is not a file.'.format(fn_csv))
+        with open(fn_csv, newline='') as f_csv:
+            epireader = csv.reader(f_csv, delimiter=',')
+            for epi in epireader:
+                print(epi)
+        return self
 
-    # check already
-    if path.isfile(fn_title):
-        print('Already reversed')
-        return True
+    def reverse(self):
+        def _get_meta_title(fn_xml):
+            title = ''
+            with open(fn_xml) as inf:
+                root = etree.parse(inf).getroot()
+                titles = list(filter(lambda c: c.tag == 'bookname', root))
+                if len(titles) != 1:
+                    return False
+                title = titles[0].text.replace('/', '.')
+                title = title[1:] if title[0] == '.' else title
+                # print(title)
+            return title
 
-    # reverse pdf
-    with open(fn_dat, mode='rb') as inf:
-        header = inf.read()
-    with open(fn_pdf, mode='rb') as inf:
-        data = inf.read()
+        def _reverse_pdf(b):
+            if not path.isdir(path.join(EXTRACTS_PATH, b)):
+                raise NotADirectoryError('{} is not a dir.'.format(b))
+            fn_pdf = path.join(EXTRACTS_PATH, b, b + '.pdf')
+            fn_dat = path.join(EXTRACTS_PATH, b, b + '.pdf.dat')
+            fn_xml = path.join(EXTRACTS_PATH, b, 'meta.xml')
+            if not path.isfile(fn_pdf):
+                raise FileNotFoundError('{} is not a file.'.format(fn_pdf))
+            if  not path.isfile(fn_dat):
+                raise FileNotFoundError('{} is not a file.'.format(fn_dat))
+            if  not path.isfile(fn_xml):
+                raise FileNotFoundError('{} is not a file.'.format(fn_xml))
 
-    # write pdf
-    with open(fn_title, mode='wb') as outf:
-        outf.write(header[::-1])
-        outf.write(data[len(header):])
+            print('looking {}: '.format(b), end='')
+            # get title
+            title = _get_meta_title(fn_xml)
+            fn_title = path.join(EXTRACTS_PATH, b, '{}-{}.pdf'.format(title, b))
 
-    print('{}-{}.pdf\n'.format(title, b))
-    return True
+            # check already
+            if path.isfile(fn_title):
+                print('[{}] Already reversed'.format(title))
+                return fn_title
+
+            # reverse pdf
+            with open(fn_dat, mode='rb') as inf:
+                header = inf.read()
+            with open(fn_pdf, mode='rb') as inf:
+                data = inf.read()
+
+            # write pdf
+            with open(fn_title, mode='wb') as outf:
+                outf.write(header[::-1])
+                outf.write(data[len(header):])
+
+            print('{}-{}.pdf'.format(title, b))
+            return fn_title
+        self.revbooks = list(map(lambda b: _reverse_pdf(b), self.bookdirs))
+        return self
 
 
 def main():
-    books = os.listdir(EXTRACTS_PATH)
-    books.sort()
-    list(map(lambda b: reverse_pdf(b), books))
+    hamirevorg = HamiRevOrg()
+    hamirevorg.reverse().get_downloaded_csv()
 
 
 if __name__ == "__main__":
